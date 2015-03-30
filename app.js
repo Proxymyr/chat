fa73b59909dd8b68825e22750f6b73192c9cddd4
Poxymyr <julien.bouedron@gmail.com>
2015-03-30 15:44:08 +0200

    Well
    This is awkward

---
+++ app.js
@@ -0,0 +1,23 @@
+var http = require("http");
+var fs = require("fs");
+var express = require("express");
+var io = require("socket.io").listen(3132);
+
+var index = fs.readFileSync('index.html');
+
+var app = express();
+
+app.get("/", function(req, res) {
+	res.end(index);
+});
+
+io.sockets.on('connection', function (socket) {
+	socket.emit("message", {username : "Moobot", message : '<img id="iii" src="http://static2.businessinsider.com/image/509802cb69bedd6209000009/nicolas-cage-will-be-in-the-expendables-3.jpg"></img>'});
+	socket.on('message', function(data) {
+		socket.emit('message', data);
+		socket.broadcast.emit('message', data);
+	})
+});
+
+
+app.listen(3131);

749051044a56495aaecad597964da98ce44e41c1
mpoiron <158507@supinfo.com>
2015-03-31 15:52:18 +0200

    Restrict FREEDOM

--- public/js/nsa.js
+++ public/js/nsa.js
@@ -1,7 +1,11 @@
-﻿function sendFreedom(){
-    var freedom = '<video preload="auto" style="min-height:336.52173913043px;width: 600px;;display:block;margin:0 auto;" width="400" loop="" autoplay="autoplay"><source src="http://img-9gag-ftw.9cache.com/photo/azEqe2b_460sv.mp4" type="video/mp4"></video>';
+﻿if (username !== 'Vega' && username !== 'Poxymyr') {
+    document.getElementById('freedomButton').disabled = true;
+}
 
-    var time = Date.now();
-    var data = { 'username': username, 'avatar': avatar, 'message': freedom, 'time' : time };
-    sendMessage(data);
+function sendFreedom(){
+        var freedom = 'GET SOME FREEDOM !!<br /><video preload="auto" style="min-height:336.52173913043px;width: 600px;;display:block;margin:0 auto;" width="400" loop="" autoplay="autoplay"><source src="http://img-9gag-ftw.9cache.com/photo/azEqe2b_460sv.mp4" type="video/mp4"></video>';
+        
+        var time = Date.now();
+        var data = { 'username': 'MURICA', 'avatar': 'http://fc04.deviantart.net/fs70/f/2012/134/d/7/profile_picture_by_freedomeagleplz-d4zr8rl.jpg', 'message': freedom, 'time' : time };
+        sendMessage(data);
 }

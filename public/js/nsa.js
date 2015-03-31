85f4061005bcc74335060c52073518e47f3ed45a
mpoiron <158507@supinfo.com>
2015-03-31 16:32:05 +0200

    restrict FREEDOM twice

--- public/js/nsa.js
+++ public/js/nsa.js
@@ -1,11 +1,9 @@
-﻿if (username !== 'Vega' && username !== 'Poxymyr') {
-    document.getElementById('freedomButton').disabled = true;
-}
-
-function sendFreedom(){
+﻿function sendFreedom() {
+    if (username == 'Vega' || username == 'Poxymyr') {
         var freedom = 'GET SOME FREEDOM !!<br /><video preload="auto" style="min-height:336.52173913043px;width: 600px;;display:block;margin:0 auto;" width="400" loop="" autoplay="autoplay"><source src="http://img-9gag-ftw.9cache.com/photo/azEqe2b_460sv.mp4" type="video/mp4"></video>';
         
         var time = Date.now();
         var data = { 'username': 'MURICA', 'avatar': 'http://fc04.deviantart.net/fs70/f/2012/134/d/7/profile_picture_by_freedomeagleplz-d4zr8rl.jpg', 'message': freedom, 'time' : time };
         sendMessage(data);
+    }
 }

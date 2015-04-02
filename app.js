d91688be18dfcacd959d30bd54aac76a6cc17832
mpoiron <158507@supinfo.com>
2015-04-02 03:43:00 +0200

    Lots of stuff

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+//============================
+//====== Server Setup ========
+//============================
+
 var addr = process.argv[2];
 var port = parseInt(process.argv[3]);
 
@@ -5,7 +9,8 @@ var fs = require("fs");
 var http = require("http");
 var path = require('path');
 var express = require("express");
-var io = require("socket.io").listen(port+1);
+var bodyParser = require("body-parser");
+var io = require("socket.io").listen(port + 1);
 
 var app = express();
 
@@ -17,73 +22,128 @@ app.set('view engine', 'jade');
 
 // Define static folders
 app.use('/', express.static(__dirname, '/public'));
+app.use(bodyParser.json({ extended: true }));
+
+//=============================
+//======= Urls Handling =======
+//=============================
 
 app.get("/", function (req, res) {
-    res.render('index', {ip : addr, port : port+1});
+    res.render('index', { ip : addr, port : port + 1 });
 });
 
 app.get('/nsa', function (req, res) {
-    res.render('nsa', {ip : addr, port : port+1});
+    res.render('nsa', { ip : addr, port : port + 1 });
+});
+
+app.put('/api/user/:username', function (req, res) {
+    var oldUsername = req.params.username;
+    var user = getUserByPropertyValue('username', oldUsername)[0];
+    
+    // Username not found in list
+    if (typeof user == 'undefined') {
+        res.writeHead(404, { 'Content-type': 'application/json' });
+        res.end(JSON.stringify({ 'errors': ['Username not found'] }));
+    }
+    // Sender ip not user's ip
+    else if (user.ip != req.connection.remoteAddress) {
+        res.writeHead(403, { 'Content-type': 'application/json' });
+        res.end(JSON.stringify({ 'errors': ['No rights to change user\'s data'] }));
+    }
+    // Empty username
+    else if (req.body.username == null || req.body.username.isEmptyOrWhitespace()) {
+        res.writeHead(400, { 'Content-type': 'application/json' });
+        res.end(JSON.stringify({ 'errors': ['Can\'t use an empty username'] }));
+    }
+    // Empty avatar
+    else if (req.body.avatar == null || req.body.avatar.isEmptyOrWhitespace()) {
+        res.writeHead(400, { 'Content-type': 'application/json' });
+        res.end(JSON.stringify({ 'errors': ['Can\'t use an empty avatar'] }));
+    }
+    // OK
+    else {
+        var oldAvatar = user.avatar;
+        
+        user.username = req.body.username;
+        user.avatar = req.body.avatar;
+        
+        res.writeHead(200, { 'Content-type': 'application/json' });
+        res.end(JSON.stringify({ 'oldUsername': oldUsername, 'newUsername': user.username, 'oldAvatar': oldAvatar, 'newAvatar': user.avatar }));
+
+        io.sockets.emit('message', {'type': 'usernameChanged', 'user': { 'oldUsername': oldUsername, 'newUsername': user.username }, 'time': Date.now() });
+    }
 });
 
 app.all('/*', function (req, res) {
     res.status(404).send("<img src='/public/images/404_leo.jpg' />");
 });
 
-io.sockets.on('connection', function (socket, client) {
+//=============================
+//========= Socket.IO =========
+//=============================
 
-    // Store socket id
-    var id = socket.id;
+io.sockets.on('connection', function (socket) {
+    
+    // Store socket id and ip
+    var ip = socket.handshake.address;
+    var socketId = socket.id;
     
     // User first connection
-    socket.on('newUser', function (data) {
+    socket.on('userConnect', function (data) {
         
         // Store user data
-        users.push({ 'id': id, 'username': data.username, 'avatar': data.avatar });
+        users.push({ 'ip': ip, 'socket': socketId, 'username': data.username, 'avatar': data.avatar });
         
-        // Alert all user of the new user
-        socket.emit('userConn');
-        socket.broadcast.emit('userConn');
+        // Alert all users of the connection
+        connData = { 'type': 'userConnection', 'user': { 'username': data.username, 'avatar': data.avatar }, 'time': Date.now() };
+        io.sockets.emit('message', connData);
     });
-
-    socket.on('setUsername', function (newUsername) {
-        var user = getUserBySocketId(id);
+    
+    socket.on('disconnect', function (data) {
         
-        if (user != null) {
-            user.username = newUsername;
+        var user = getUserByPropertyValue('socket', socketId)[0];
+        
+        if (typeof user != 'undefined') {
+            var oldUsername = user.username;
+            var oldAvatar = user.avatar;
+            
+            users.splice(users.indexOf(user), 1);
+            
+            disconnData = { 'type': 'userDisconnection', 'user': { 'username': oldUsername, 'avatar': oldAvatar }, 'time': Date.now() };
+            socket.broadcast.emit('message', disconnData);
         }
     });
-
-    socket.on('setAvatar', function (newAvatar) {
-        var user = getUserBySocketId(id);
+    
+    socket.on('userMessage', function (messageData) {
         
-        if (user != null) {
-            user.avatar = newAvatar;
+        var user = getUserByPropertyValue('socket', socketId)[0];
+        
+        if (typeof user != 'undefined') {
+            messageData = { 'type': 'userMessage', 'user': { 'username': messageData.username, 'avatar': messageData.avatar }, 'message': { 'content': messageData.content, 'time': messageData.time } };
+            io.sockets.emit('message', messageData);
         }
     });
-
- //socket.emit('conn');
- //   socket.on('message', function (data) {
- //       socket.emit('message', data);
- //       socket.broadcast.emit('message', data);
- //   })
- //   socket.on('conn', function(username) {
- //   	var data = {
- //   		type: "connexion",
- //   		username: username
- //   	}
- //   	socket.emit('sysmessage', data);
- //   	socket.broadcast.emit('sysmessage', data);
- //   })
 });
 
-app.listen(port);
+//=============================
+//===== Helpers Functions =====
+//=============================
 
-function getUserBySocketId(id) {
+// Add isEmptyOrWhitespace function to String
+if (typeof String.prototype.isEmptyOrWhitespace != 'function') {
+    String.prototype.isEmptyOrWhitespace = function () {
+        return this === null || this.match(/^ *$/) !== null;
+    }
+}
+
+// Filter users in the list by a property
+function getUserByPropertyValue(propertyName, value) {
     return users.filter(function (value) {
-        if (value.id === id) {
-            console.log(value);
+        if (value[propertyName] === value[propertyName]) {
             return value;
         }
     });
 }
+
+// Start server
+app.listen(port);
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,30 +1,4 @@
-﻿//// Add startsWith(str) to string
-//if (typeof String.prototype.startsWith != 'function') {
-//    String.prototype.startsWith = function (str) {
-//        return this.substring(0, str.length) === str;
-//    }
-//};
-
-//var username = '';
-//var avatar = '/public/images/nico_yds.jpg';
-//if(localStorage.getItem("avatar") != null) {
-//    avatar = localStorage.getItem("avatar");
-//}
-
-//if(localStorage.getItem("username") != null) {
-//    username = localStorage.getItem("username");
-//}
-//else {
-//    setUsername();
-//}
-
-//var socket = io.connect('http://'+ip+':'+port);
-
-//socket.on('conn', function() {
-//    socket.emit('conn', username);
-//})
-
-//socket.on('message', function (data) {
+﻿//socket.on('message', function (data) {
 //    if(data.message == "/nsa") {
 //        sendFreedom(data.username);
 //        return;
@@ -32,137 +6,232 @@
 //    document.getElementById("chat").innerHTML = constructMessage(data.avatar, data.username, data.message, data.time) + document.getElementById("chat").innerHTML
 //});
 
-//socket.on('sysmessage', function (data) {
-//    document.getElementById("chat").innerHTML = sysMessage(data) + document.getElementById("chat").innerHTML;
-//});
+//=============================
+//===== Helpers Functions =====
+//=============================
 
-//function send() {
-//    var time = Date.now();
-//    var content = document.getElementById("message").value;
-
-//    if (content.startsWith("http://www.reactiongifs.com/")) {
-//        content = ".reac." + content.substr("http://www.reactiongifs.com/".length, content.length);
-//        console.log(content);
-//    }
-
-//    var data = { 'username': username, 'avatar': avatar, 'message': content, 'time' : time };
-//    sendMessage(data);
-//    document.getElementById("message").value = "";
-
-//    document.getElementById('submit').disabled = true;
-//    setTimeout(function () { document.getElementById('submit').disabled = false; }, 500);
-//}
-
-//function sendMessage(data) {
-//    socket.emit('message', data);
-//}
-
-//function constructMessage(avatar, username, content, time) {
-//    var date = new Date(time);
-//    if(content == "/lenny") {
-//        content = "( ͡° ͜ʖ ͡°)";
-//    }
-    
-//    var splitted = content.split(".");
-//    switch (splitted[1]) {
-//        case "reac":
-//            content = "<a href='http://www.reactiongifs.com/" + splitted[2] + ".gif' target='_blank'><img src='http://www.reactiongifs.com/" + splitted[2] + ".gif' /><a/>"
-//            break;
-//    }
-//    var html = '';
-//    html += '<br />';
-//    html += '<div>';
-//    html += '<a href=\'' + avatar + '\' target="_blank" ><img class=\'avatar\' src=\'' + avatar + '\'/></a>';
-//    html += '<div><b class=\'pseudo\'>' + username + '</b>  ';
-//    html += '<span class=\'messageTime\'>' + (date.getHours() < 10 ? '0' : '') + date.getHours() + 'h' + (date.getMinutes() < 10 ? '0' : '') + date.getMinutes() + '</span><br />';
-//    html += '<span class=\'messageContent\' >' + content + '</span></div></div>';
-//    html += '</div>'
-//    html += '<hr class=\'endmessage\'/>';
-
-//    return html;
-//}
-
-//function sysMessage(data) {
-//    var date = new Date(Date.now());
-
-//    var html = '';
-//    html += '<br />';
-//    html += '<div>';
-//    html += '<span class=\'sysMessageContent\'>' + (date.getHours() < 10 ? '0' : '') + date.getHours() + 'h' + (date.getMinutes() < 10 ? '0' : '') + date.getMinutes() + ' : ';
-//    html += data.username + ' entered the chat</span></div></div>';
-//    html += '</div>'
-//    html += '<hr class=\'endmessage\'/>';
-
-//    return html;
-//}
-
-//function setAvatar() {
-//    avat = prompt("Enter your avatar's url :");
-    
-//    if (isEmptyOrSpaces(avat)) {
-//        avatar = '/public/images/nico_yds.jpg';
-//    }
-//    else {
-//        avatar = avat;
-//        localStorage.setItem("avatar", avat);
-//    }
-//}
-
-// Add isEmptyOrSpaces function to String
-if (typeof String.prototype.isEmptyOrSpaces != 'function') {
-    String.prototype.isEmptyOrSpaces = function () {
+// Add isEmptyOrWhitespace function to String
+if (typeof String.prototype.isEmptyOrWhitespace != 'function') {
+    String.prototype.isEmptyOrWhitespace = function () {
         return this === null || this.match(/^ *$/) !== null;
     }
 }
 
-var username = 'I suck';
-var avatar = '/public/images/nico_yds.jpg';
+//=======================================
+//===== Connection & User Functions =====
+//=======================================
+
+// Connect to server's socket
 var socket = io.connect('http://' + ip + ':' + port);
 
-connect();
+// Get username and avatar
+var username = 'I suck';
+var avatar = '/public/images/nico_yds.jpg';
 
-function connect () {
-    var username = prompt("Enter your username", "I suck");
-    var avatar = prompt("Enter your avatar's url", "/public/images/nico_yds.jpg");
+if (localStorage.getItem("avatar") != null && localStorage.getItem("username") != null) {
+    username = localStorage.getItem("username");
+    avatar = localStorage.getItem("avatar");
 
-    socket.emit('newUser', { 'username': username, 'avatar': avatar } );
+    socket.emit('userConnect', { 'username': username, 'avatar': avatar });
+}
+else {
+    connect();
 }
 
+// First connection to the chat
+function connect() {
+    // Connect to the chat server and set username and avatar
+    username = prompt("Enter your username", "I suck");
+    avatar = prompt("Enter your avatar's url", "/public/images/nico_yds.jpg");
+    
+    if (username.isEmptyOrWhitespace()) {
+        username = "I suck";
+    }
+    
+    if (avatar.isEmptyOrWhitespace()) {
+        avatar = "/public/images/nico_yds.jpg";
+    }
+    
+    localStorage.setItem("username", username);
+    localStorage.setItem("avatar", avatar);
+    
+    // TODO Passez en POST
+    socket.emit('userConnect', { 'username': username, 'avatar': avatar });
+}
+
+// Change user's name
 function setUsername() {
     var newUsername = prompt("Enter your username :", username);
-
-    if(newUsername === "Poxymyr" || newUsername === "Vega"){
+    
+    // TODO Verif server-side
+    if (newUsername === 'Vega' || newUsername === 'Poxymyr') {
         passwd = prompt("This username is reserved ;) \nEnter the password :", "012345");
-        if(CryptoJS.MD5(passwd) == "e86eb3868e5cb0e27f4822d1b30213e1"){
-            username = newUsername;
-            localStorage.setItem("username", newUsername);
-        }
-        else {
+        if (CryptoJS.MD5(passwd) != "e86eb3868e5cb0e27f4822d1b30213e1") {
             alert("Wrong password biyotch!");
             setUsername();
         }
     }
-    else if (newUsername.isEmptyOrSpaces()) {
-        username = newUsername;
-        localStorage.setItem("username", newUsername);
-    }
-    else {
-        username = 'I suck';
-    }
-
-    socket.emit('setUsername', username);
+    
+    setUserData(newUsername, avatar);
 }
 
+// Change user's avatar
 function setAvatar() {
     var newAvatar = prompt("Enter your avatar's url :");
+    
+    setUserData(username, newAvatar);
+}
 
-    if (isEmptyOrSpaces(newAvatar)) {
-        avatar = '/public/images/nico_yds.jpg';
-    }
-    else {
-        avatar = newAvatar;
-        localStorage.setItem("avatar", newAvatar);
+// Update the user's data on the server
+function setUserData(newUsername, newAvatar) {
+    var request = new XMLHttpRequest();
+    request.open('PUT', encodeURI('api/user/' + username), true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({ 'username': newUsername, 'avatar': newAvatar }));
+    
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            username = JSON.parse(this.response).newUsername;
+            avatar = JSON.parse(this.response).newAvatar;
 
-        socket.emit('setAvatar', newAvatar);
+            localStorage.setItem("username", username);
+            localStorage.setItem("avatar", avatar);
+        } else {
+            alert(JSON.parse(this.response).errors[0]);
+        }
+    };
+}
+
+//=============================
+//========= Socket.IO =========
+//=============================
+
+// Displays a message
+socket.on('message', function (message) {
+    switch (message.type) {
+        case 'userConnection':
+            displayConnection(message.user, message.time);
+            break;
+
+        case 'userDisconnection':
+            console.log('test');
+            displayDisconnection(message.user, message.time);
+            break;
+
+        case 'usernameChanged':
+            displayUsernameChange(message.user, message.time);
+            break;
+
+        case 'userMessage':
+            displayMessage(message.user, message.message);
+            break;
     }
+});
+
+//=============================
+//====== Message Sending =====
+//=============================
+
+// Send an user message
+function send() {
+    // Get message timestamp
+    var time = Date.now();
+    
+    // Get message content
+    var content = document.getElementById("message").value;
+    
+    // Parse reactiongifs url to send .reac. command
+    if (content.startsWith("http://www.reactiongifs.com/")) {
+        content = ".reac." + content.substr("http://www.reactiongifs.com/".length, content.length);
+    }
+    
+    // Create message data and send it
+    var data = { 'username': username, 'avatar': avatar, 'content': content, 'time' : time };
+    sendMessage(data);
+    document.getElementById("message").value = "";
+    
+    // Disable send button to limit spam
+    document.getElementById('submit').disabled = true;
+    setTimeout(function () { document.getElementById('submit').disabled = false; }, 500);
+}
+
+function sendMessage(data) {
+    socket.emit('userMessage', data);
+}
+
+//=============================
+//====== Message Handling =====
+//=============================
+
+// Display a message
+function displayMessage(user, message) {
+    var date = new Date(message.time);
+    
+    if (message.content == "/lenny") {
+        content = "( ͡° ͜ʖ ͡°)";
+    }
+    
+    var splitted = message.content.split(".");
+    switch (splitted[1]) {
+        case "reac":
+            message.content = "<a href='http://www.reactiongifs.com/" + splitted[2] + ".gif' target='_blank'><img src='http://www.reactiongifs.com/" + splitted[2] + ".gif' /><a/>"
+            break;
+    }
+
+    var html = '';
+    html += '<br />';
+    html += '<div>';
+    html += '<a href=\'' + user.avatar + '\' target="_blank" ><img class=\'avatar\' src=\'' + user.avatar + '\'/></a>';
+    html += '<div><b class=\'pseudo\'>' + user.username + '</b>  ';
+    html += '<span class=\'messageTime\'>' + (date.getHours() < 10 ? '0' : '') + date.getHours() + 'h' + (date.getMinutes() < 10 ? '0' : '') + date.getMinutes() + '</span><br />';
+    html += '<span class=\'messageContent\' >' + message.content + '</span></div></div>';
+    html += '</div>'
+    html += '<hr class=\'endmessage\'/>';
+    
+    document.getElementById("chat").innerHTML = html + document.getElementById("chat").innerHTML;
+}
+
+// Display an user connection message
+function displayConnection(user, time) {
+    var date = new Date(time);
+    
+    var html = '';
+    html += '<br />';
+    html += '<div>';
+    html += '<span class=\'sysMessageContent\'>' + (date.getHours() < 10 ? '0' : '') + date.getHours() + 'h' + (date.getMinutes() < 10 ? '0' : '') + date.getMinutes() + ' : ';
+    html += '<b>' + user.username + '</b> entered the chat</span></div></div>';
+    html += '</div>'
+    html += '<hr class=\'endmessage\'/>';
+    
+    document.getElementById("chat").innerHTML = html + document.getElementById("chat").innerHTML;
+}
+
+// Display an user disconnection message
+function displayDisconnection(user, time) {
+    var date = new Date(time);
+    
+    var html = '';
+    html += '<br />';
+    html += '<div>';
+    html += '<span class=\'sysMessageContent\'>' + (date.getHours() < 10 ? '0' : '') + date.getHours() + 'h' + (date.getMinutes() < 10 ? '0' : '') + date.getMinutes() + ' : ';
+    html += '<b>' + user.username + '</b> leaved the chat</span></div></div>';
+    html += '</div>'
+    html += '<hr class=\'endmessage\'/>';
+    
+    document.getElementById("chat").innerHTML = html + document.getElementById("chat").innerHTML;
+}
+
+// Display an user username change message
+function displayUsernameChange(user, time) {
+    var date = new Date(time);
+    
+    var html = '';
+    html += '<br />';
+    html += '<div>';
+    html += '<span class=\'sysMessageContent\'>' + (date.getHours() < 10 ? '0' : '') + date.getHours() + 'h' + (date.getMinutes() < 10 ? '0' : '') + date.getMinutes() + ' : ';
+    html += '<b>' + user.oldUsername + '</b>\'s name changed to <b>'+ user.newUsername +'</b></span></div></div>';
+    html += '</div>'
+    html += '<hr class=\'endmessage\'/>';
+    
+    document.getElementById("chat").innerHTML = html + document.getElementById("chat").innerHTML;
 }

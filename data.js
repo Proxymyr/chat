dcee1a852a7412c98e31780a13d0a453b4cc2f70
mpoiron <maxime.rp@hotmail.fr>
2015-04-12 21:50:54 +0200

    Merged mongoose usage and schema in one file Add an user to the DB on connection (test step)

--- data.js
+++
@@ -1,8 +0,0 @@
-var	mongoose = require('./getMongoose.js').mongoose;
-var DataSchema = mongoose.Schema({
-			'name': String,
-			'value':String
-		}),
-DataModel = mongoose.model('Data', DataSchema);
-
-exports.Data = DataModel;
